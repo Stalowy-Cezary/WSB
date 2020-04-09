5624cce7c19a1c1b0ad001fc5920902070e533d2
Stalowy-Cezary <63396078+Stalowy-Cezary@users.noreply.github.com>
2020-04-09 16:41:54 +0200

    Update zadanka.java

--- zadanka.java
+++ zadanka.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class zadanka {
+public class Zadanie6 {
 public static void main(String[] args)
 {
 int zycie = 5;
@@ -7,31 +7,31 @@ String haslo = "polska"; //okreslam haslo do wisielca
 char[] tablicaHaslo = haslo.toCharArray(); //zmieniam haslo na tablice znakow
 for(;;)
 {
-	Scanner litera = new Scanner(System.in);
-	String litera1 = litera.nextLine();  // zgaduje literke
+	Scanner scan = new Scanner(System.in);
+	String litera = scan.nextLine();  // zgaduje literke
 	//vvvv jesli wpisuje literke ktora pasuje, dostaje odpowiedz na ktorym jest miejscu
-	if(haslo.equals(litera1))  
+	if(haslo.equals(litera))  
 	{
 		System.out.println("zwyciestwo!");
 		break;
 	}
 	//vvvv sprawdzam kolejnosc literki
-	else if(haslo.contains(litera1))
-		{
-		System.out.println(litera1+" jest na "+(haslo.indexOf(litera1)+1)+" miejscu"); 
-		}
+	else if(haslo.contains(litera))
+	{
+		System.out.println(litera+" jest na "+(haslo.indexOf(litera)+1)+" miejscu"); 
+	}
 	//vvvv jesli pomyle sie 5 razy przegrywam
 	else
-		{
+	{
 		zycie--;
 		System.out.println("Zostalo "+(zycie)+" zyc");
-			if(zycie==0)
-			{
-				System.out.println("Przegrales");
-				break;
-			}
-	
+		if(zycie==0)
+		{
+			System.out.println("Przegrales");
+			break;
 		}
+	
+	}
 }
 }
 }
